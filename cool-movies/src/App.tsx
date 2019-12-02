0ec9a7a748d917517407a9af58a2d1bdcfdd0452
Emanuel Deckel <emanuel.deckel@turner.com>
2019-12-02 11:59:29 +0200

    initial

---
+++ cool-movies/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      app
+    </div>
+  );
+}
+
+export default App;
---
+++ cool-movies/src/constants.ts
@@ -0,0 +1,9 @@
+// Top Rated Movies
+// https://api.themoviedb.org/3/movie/top_rated?api_key={api_key}&language=en-US&page=1
+// Search Movies
+// https://api.themoviedb.org/3/search/movie?api_key={api_key}&language=en-US&page=1&include_adult=false
+// Movie Details
+// https://api.themoviedb.org/3/movie/{movie_id}?api_key={api_key}&language=en-US
+
+export const API_KEY = "6723a43a0d554a924752c10b71b5b401";
+export const PICS_BASE_URL = "https://image.tmdb.org/t/p/w300/";
