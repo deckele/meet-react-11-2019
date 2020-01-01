c722f29c8bf3fe2b6758d4ce77eb32629051b131
Deckel, Emanuel (ed038p) <ed038p@intl.att.com>
2020-01-01 11:21:06 +0200

    added blog content

--- blog/src/App.tsx
+++ blog/src/App.tsx
@@ -1,24 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Route, Switch, NavLink, Redirect, Prompt } from "react-router-dom";
 
+const Home = () => {
+  const [ input, setInput ] = useState("");
+  return (
+    <>
+      <Prompt when={!!input} message={(location) => `are you sure you want to go to: ${location.pathname}`} />
+      <div>Home</div>
+      <input value={input} onChange={(e) => setInput(e.target.value)} type="text"/>
+    </>
+  );
+}
 const App: React.FC = () => {
+  const activeNavStyle = {backgroundColor: "green"};
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavLink activeStyle={activeNavStyle} to="/home" exact>Home</NavLink>
+      <NavLink activeStyle={activeNavStyle} to="/about" exact>About</NavLink>
+      <NavLink activeStyle={activeNavStyle} to="/blog" exact>Blog</NavLink>
+      <Switch>
+        <Route path="/home" exact>
+          <Home />
+        </Route>
+        <Route path="/home">
+          <Redirect to="/home" />
+        </Route>
+        <Route path="/about" exact>
+          <div>About</div>
+        </Route>
+        <Route path="/about">
+          <Redirect to="/about" />
+        </Route>
+        <Route path="/blog" exact>
+          <div>Blog</div>
+        </Route>
+        <Route path="/blog">
+          <Redirect to="/blog" />
+        </Route>
+        <Route>
+          <Redirect to="/home" />
+        </Route>
+      </Switch>
     </div>
   );
 }
